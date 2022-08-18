77fa1761e5acd27fced5d83f2b2d7f3df9735be5
Julian <palcojulian@outlook.com>
2022-08-18 14:51:50 -0500

    Sexto avance

--- todo/auth.py
+++ todo/auth.py
@@ -34,6 +34,33 @@ def register():
             db.commit()
         
         return redirect(url_for('auth.login'))
-        
+
     flash(error)
     return render_template('auth/register.html')
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db, c = get_db()
+        error = None
+
+        c.execute(
+            'select * from user where username = %s',(username,)
+        )
+
+        user = c.fetchone()
+
+        if user is None:
+            error = 'Usuario y/o contraseña invalido'
+        elif not check_password_hash(user['password'], password):
+            error = 'Usuario y/o contraseña invalido' 
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+    
+    flash(error)
+    return render_template('auth/login.html')

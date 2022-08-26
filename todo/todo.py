123669f25004745ae063200142221b5ab5c30c9b
Julian <palcojulian@outlook.com>
2022-08-25 23:02:52 -0500

    Funcion para actualizar lista de tareas completa

--- todo/todo.py
+++ todo/todo.py
@@ -1,3 +1,4 @@
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -47,10 +48,44 @@ def create():
         
     return render_template('todo/create.html')
 
+def get_todo(id):
+    db, c = get_db()
+    c.execute(
+        'select t.id, t.description, t.completed, t.create_by, created_at, u.username from todo t join user u on t.create_by = u.id where t.id = %s',
+        (id,)
+    )
+
+    todo = c.fetchone()
+
+    if todo is None:
+        abort(404, 'El todo de id {0} no existe'.format(id))
+
+    return todo
+
 @bp.route('/<int:id>/update', methods=['GET', 'POST'])
 @login_required
 def update(id):
-    
+    todo = get_todo(id)
+
+    if request.method == 'POST':
+        description = request.form['description']
+        completed = True if request.form.get('completed') == 'on' else False
+        error = None
+
+        if not description:
+            error = 'La descripción es requerida'
+
+        if error is not None:
+            flash(error)
+        else:
+            db, c = get_db()
+            c.execute(
+                'update todo set description = %s, completed = %s where id = %s',
+                (description, completed, id)
+            )
+            db.commit()
+            return redirect(url_for('todo.index'))
+
     return render_template('todo/update.html', todo=todo)
 
 @bp.route('/<int:id>/delete', methods=['POST'])
